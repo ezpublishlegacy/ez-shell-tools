daac02506c12ac5fff18a8e421a2f513d212c620
Steven E. Bailey <github@leidentech.com>
2013-02-05 18:44:16 +0100

    update eZPublish 3.5.1 to 4.4.0

--- convertscript.sh
+++ convertscript.sh
@@ -2,25 +2,58 @@
 #Copyright 2007-2012 Leidentech All rights reserved.
 #license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 
+dbversion ()
+{
+dbversion=`php -r "require 'autoload.php';
+echo eZPublishSDK::databaseVersion(false);"`
+echo $dbversion
+}
+version ()
+{
+version=`php -r "require 'autoload.php';
+echo eZPublishSDK::version();"`
+echo $version
+}
+
 sites ()
 {
-        echo "en fr"
+sites=`php -r "require 'autoload.php';
+\\$ini = eZINI::instance();
+\\$sitelist=\\$ini->variable( 'SiteSettings', 'SiteList' );
+foreach(\\$sitelist as \\$site) echo \\$site.' ';"`
+echo $sites
 }
+
 adminsites ()
 {
-        echo "admin admin2"
+for siteaccess in `sites`
+do
+if [ `echo $siteaccess|grep -ic "admin"` -ne 0 ]
+then
+        echo $siteaccess
+fi
+done
 }
+
 dbinfo ()
 {
-cat <<-EOF
-<database> <user> <password>
-<database2> <user2> <password2>
-EOF
+dbinfo=`php -r "require 'autoload.php';
+\\$script = eZScript::instance( array( 'debug-message' => '', 'use-session' => true, 'use-modules' => true, 'use-extensions' => true ) );
+\\$script->startup();
+\\$script->setUseSiteAccess( '${1}' );
+\\$script->initialize();
+\\$ini = eZINI::instance();
+\\$database=\\$ini->variable( 'DatabaseSettings', 'Database' );
+\\$user=\\$ini->variable( 'DatabaseSettings', 'User' );
+\\$password=\\$ini->variable( 'DatabaseSettings', 'Password' );
+\\$script->shutdown();
+echo \\$database.' '.\\$user.' '.\\$password;"`
+echo $dbinfo;
 }
 newtype ()
 {
 echo "Converting all tables to InnoDB"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
 	php bin/php/ezconvertmysqltabletype.php --host=localhost  --user=$user --database=$db --password=$password --newtype=InnoDB 2>/dev/null
 done
@@ -28,7 +61,7 @@ done
 upto352 ()
 {
 echo "Starting 3.5.2 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
         mysql -u $user --password=$password $db < update/database/mysql/3.5/dbupdate-3.5.1-to-3.5.2.sql|egrep -v "Warning|Notice"
 done
@@ -37,7 +70,7 @@ newtype
 upto36 ()
 {
 echo "Starting 3.6 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
         mysql -f -u $user --password=$password $db < update/database/mysql/3.6/dbupdate-3.5.2-to-3.6.0.sql
 done
@@ -57,7 +90,7 @@ do #this has to be run on ALL site accesses
 	echo "Updating eztimetype"
 	php update/common/scripts/updateeztimetype.php -s $siteaccess
 done
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
 	echo "Updating crc"
 	php update/common/scripts/updatecrc32.php $user:$password:$db:mysql
@@ -66,7 +99,7 @@ done
 upto38 ()
 {
 echo "Starting 3.8 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
         mysql -f -u $user --password=$password $db < update/database/mysql/3.8/dbupdate-3.6.0-to-3.8.0.sql
 done
@@ -83,7 +116,7 @@ done
 upto39 ()
 {
 echo "Starting 3.9 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
 	for sql in update/database/mysql/3.9/*.sql
 	do
@@ -125,7 +158,7 @@ done
 upto310 ()
 {
 echo "Starting 3.10 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
 	for sql in update/database/mysql/3.10/*.sql
 	do
@@ -166,7 +199,7 @@ done
 upto400 ()
 {
 echo "Starting 4.0 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
 	for sql in update/database/mysql/4.0/*.sql
 	do
@@ -199,10 +232,11 @@ done
 upto401 ()
 {
 echo "Starting 4.0.1 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
-	echo "Adding ezurlalias_migration schema"
-	mysql -u $user --password=$password $db < extension/ezurlaliasmigration/sql/mysql/schema.sql
+#DOING URL ALIASES LATER
+	#echo "Adding ezurlalias_migration schema"
+	#mysql -u $user --password=$password $db < extension/ezurlaliasmigration/sql/mysql/schema.sql
 	for sql in update/database/mysql/4.0/*.sql
 	do
 		echo "running $sql"
@@ -231,52 +265,54 @@ do
 	/usr/bin/php5 update/common/scripts/4.0/fixobjectremoteid.php -s $siteaccess
 	echo "Updating binary files"
 	/usr/bin/php5 update/common/scripts/4.0/updatebinaryfile.php -s $siteaccess
-	echo "Creating migration table"
+	#echo "Creating migration table"
 	#php5 extension/ezurlaliasmigration/scripts/migrate.php --create-migration-table
-	echo "migrating url aliases"
-	php5 extension/ezurlaliasmigration/scripts/migrate.php --migrate
+	#echo "migrating url aliases"
+	#php5 extension/ezurlaliasmigration/scripts/migrate.php --migrate
 	#mysql -u $user --password=$password $db -e "UPDATE ezurlalias SET is_imported=0; TRUNCATE ezurlalias_ml;"
-	echo "Updating nice urls"
-        /usr/bin/php5 bin/php/updateniceurls.php --import --fetch-limit=100 -s $siteaccess
-        php extension/ezurlaliasmigration/scripts/migrate.php --restore
+	#echo "Updating nice urls"
+        #/usr/bin/php5 bin/php/updateniceurls.php --import --fetch-limit=100 -s $siteaccess
+        #php extension/ezurlaliasmigration/scripts/migrate.php --restore
 	echo "Updating tipafriend"
 	/usr/bin/php5 update/common/scripts/4.0/updatetipafriendpolicy.php -s $siteaccess -l admin -p $password
 	#echo "Updating vatcountries" Doesn't exist, but documented ?!?
-	#/usr/bin/php5 update/common/scripts/4.0/updatevatcountries.php -s $siteaccess
+	/usr/bin/php5 update/common/scripts/4.0/updatevatcountries.php -s $siteaccess
 done
 }
 upto403 ()
 {
 echo "Starting 4.0.3 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
-	mysql -f -u $user --password=$password $db < update/database/mysql/4.0/dbupdate-4.0.1-to-4.0.2.sql
-	mysql -f -u $user --password=$password $db < update/database/mysql/4.0/dbupdate-4.0.2-to-4.0.3.sql
+	#mysql -f -u $user --password=$password $db < update/database/mysql/4.0/dbupdate-4.0.1-to-4.0.2.sql
+	#mysql -f -u $user --password=$password $db < update/database/mysql/4.0/dbupdate-4.0.2-to-4.0.3.sql
         continue
 done
-for siteaccess in `adminsites`
-do
-	php5 extension/ezurlaliasmigration/scripts/migrate.php --create-migration-table -s $siteaccess
-	php5 extension/ezurlaliasmigration/scripts/migrate.php --migrate -s $siteaccess
-done
-dbinfo|while read db user password
-do
-	mysql -u $user --password=$password $db -e "UPDATE ezurlalias SET is_imported=0; TRUNCATE ezurlalias_ml;"
-done
+
+#THIS WILL BE DONE BELOW
+#for siteaccess in `adminsites`
+#do
+	#php5 extension/ezurlaliasmigration/scripts/migrate.php --create-migration-table -s $siteaccess
+	#php5 extension/ezurlaliasmigration/scripts/migrate.php --migrate -s $siteaccess
+#done
+#dbinfo|while read db user password
+#do
+	#mysql -u $user --password=$password $db -e "UPDATE ezurlalias SET is_imported=0; TRUNCATE ezurlalias_ml;"
+#done
 newtype
-echo "Starting scripts"
-for siteaccess in `adminsites`
-do
-	php5 update/common/scripts/4.0/initurlaliasmlid.php -s $siteaccess
-	echo "Updating nice urls"
-        /usr/bin/php5 bin/php/updateniceurls.php --import --fetch-limit=100 -s $siteaccess
-        php extension/ezurlaliasmigration/scripts/migrate.php --restore -s $siteaccess
-done
+#echo "Starting scripts"
+#for siteaccess in `adminsites`
+#do
+	#php5 update/common/scripts/4.0/initurlaliasmlid.php -s $siteaccess
+	#echo "Updating nice urls"
+        #/usr/bin/php5 bin/php/updateniceurls.php --import --fetch-limit=100 -s $siteaccess
+        #php extension/ezurlaliasmigration/scripts/migrate.php --restore -s $siteaccess
+#done
 }
 upto410 ()
 {
 echo "Starting 4.1.0 upgrade"
-dbinfo|while read db user password
+dbinfo `adminsites`|while read db user password
 do
 	for sql in update/database/mysql/4.1/*.sql
 	do
@@ -318,7 +354,9 @@ do
 	#echo "Doing updateimagesystem.php"
 	#Undocumented!!
 	#correctxmlalign.php
+	php5 update/common/scripts/4.1/correctxmlalign.php -s $siteaccess
 	#fixnoderemoteid.php
+	php5 update/common/scripts/4.1/fixnoderemoteid.php -s $siteaccess
 done
 }
 upto420 ()
@@ -327,8 +365,10 @@ echo "Starting 4.2.0 upgrade"
 #If you are upgrading to the 4.2 series of eZ Publish for the first time, and the installation at hand has been running since prior to eZ Publish 3.3 then you need to run the updateimagesystem.php script before running any of the dbupdate scripts for version 4.2.
 #php5 update/common/scripts/4.1/updateimagesystem.php -s $siteaccess
 
-dbinfo|while read db user password
+echo "Adding ezurlalias_migration schema"
+dbinfo `adminsites`|while read db user password
 do
+	mysql -u $user --password=$password $db < extension/ezurlaliasmigration/sql/mysql/schema.sql
 	for sql in update/database/mysql/4.2/*.sql
 	do
 		echo "running $sql"
@@ -350,6 +390,72 @@ echo "Starting scripts"
 for siteaccess in `adminsites`
 do
 	php5 update/common/scripts/4.2/fixorphanimages.php -s $siteaccess
+	echo "migrating url aliases"
+	php5 extension/ezurlaliasmigration/scripts/migrate.php --migrate
+	#Shouldn't need this mysql -u $user --password=$password $db -e "UPDATE ezurlalias SET is_imported=0; TRUNCATE ezurlalias_ml;"
+	echo "Updating nice urls"
+        /usr/bin/php5 bin/php/updateniceurls.php --import --fetch-limit=100 -s $siteaccess
+        php extension/ezurlaliasmigration/scripts/migrate.php --restore
+done
+}
+upto430 ()
+{
+echo "Starting 4.3.0 upgrade"
+
+dbinfo `adminsites`|while read db user password
+do
+	for sql in update/database/mysql/4.3/dbupdate-4.2.0-to-4.3.0.sql
+	do
+		echo "running $sql"
+        	mysql -f -u $user --password=$password $db < $sql
+		if [ $? -ne 0 ]
+		then
+			echo $sql failed!!!
+			sqlfail=true
+			exit
+		fi
+		if [ -n "$sqlfail" ]
+		then
+        		echo "FIX THE DB ERROR FIRST"
+			exit
+		fi
+	done
+done
+echo "Starting scripts"
+for siteaccess in `adminsites`
+do
+	echo "Updating Node Assignments"
+	php5 update/common/scripts/4.3/updatenodeassignment.php  --allow-root-user
+done
+}
+upto440 ()
+{
+echo "Starting 4.4.0 upgrade"
+
+dbinfo `adminsites`|while read db user password
+do
+	for sql in update/database/mysql/4.4/*.sql
+	do
+		echo "running $sql"
+        	mysql -f -u $user --password=$password $db < $sql
+		if [ $? -ne 0 ]
+		then
+			echo $sql failed!!!
+			sqlfail=true
+			exit
+		fi
+		if [ -n "$sqlfail" ]
+		then
+        		echo "FIX THE DB ERROR FIRST"
+			exit
+		fi
+	done
+done
+echo "Starting scripts"
+for siteaccess in `adminsites`
+do
+	echo "Updating Node Assignments"
+	php5 update/common/scripts/4.4/updatesectionidentifier.php --allow-root-user
 done
 }
 
@@ -358,6 +464,9 @@ done
 # MAIN #
 #      #
 ########
+dbinfo `adminsites`
+echo Database Version: `dbversion`
+echo File System Version: `version`
 case $1 in
 	1) upto352;;
 	2) upto36;;
@@ -369,6 +478,8 @@ case $1 in
 	8) upto403;;
 	9) upto410;;
 	10) upto420;;
+	11) upto430;;
+	12) upto440;;
 	*) cat <<EOF
 input needed:
 	1 upto352 you should be in the 3.6 directory
@@ -381,6 +492,8 @@ input needed:
 	8 upto403 you should be in the 4.0 directory
 	9 upto410 you should be in the 4.0 directory
 	10 upto420 you should be in the 4.0 directory
+	11 upto430 you should be in the 4.3 directory
+	12 upto440 you should be in the 4.4 directory
 EOF
 	   ;;
 esac
